67ebb4ff28527f28641bef495b226059633eff2e
sathitsc <sathitseth.c@gmail.com>
2025-05-27 00:02:07 +0700

    Implement external command execution (Milestone 3)

---
+++ icsh_external_command.h
@@ -0,0 +1,11 @@
+/* ICCS227: Project 1: icsh
+* Name: Chinanard Sathitseth
+ * StudentID: 6481366
+ */
+
+#ifndef ICSH_EXTERNAL_COMMAND_H
+#define ICSH_EXTERNAL_COMMAND_H
+
+int run_external_command(char *args[], int *exit_code);
+
+#endif
