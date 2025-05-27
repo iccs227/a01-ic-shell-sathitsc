4f2ebbc2b6aebe3b5ffa803b3f6ae35d034ec95e
sathitsc <sathitseth.c@gmail.com>
2025-05-27 18:33:35 +0700

    Milestone 4: Signal Handler icsh_signal_handler Implement Signal Handler

---
+++ icsh_signal_handler.h
@@ -0,0 +1,14 @@
+/* ICCS227: Project 1: icsh
+* Name: Chinanard Sathitseth
+ * StudentID: 6481366
+ */
+
+#ifndef ICSH_SIGNAL_HANDLER_H
+#define ICSH_SIGNAL_HANDLER_H
+
+extern pid_t fg_pid;
+
+void signal_handler(int is_signal);
+void implement_signal_handlers();
+
+#endif
